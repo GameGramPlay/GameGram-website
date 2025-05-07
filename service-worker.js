8bb282c515d5cf01e24a5b441606bfb16a12d1ec
AEhehe <aeahb003805mg@ahbs.ae>
2025-05-07 08:00:09 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,9 @@ const urlsToCache = [
   '/style.css',
   '/manifest.json',
   '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icons/icon-512.png',
+  '/features/index.html',
+  '/about/index.html'
 ];
 
 self.addEventListener('install', event => {
